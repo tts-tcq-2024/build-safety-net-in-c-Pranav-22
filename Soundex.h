5f82fb4e6282ccbafb9c864557b97ceac91ad09b
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-07-03 09:39:11 +0530

    refactored checkcode

--- Soundex.h
+++ Soundex.h
@@ -4,33 +4,49 @@
 #include "Soundex.h"
 #include <ctype.h>
 #include <string.h>
-
 char getSoundexCode(char c)
 {
+    int i = 0;
     char res = '0';
     char chararr[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     char codearr[26] = {'0','1','2','3','0','1','2','0','0','2','2','4','5','5','0','1','2','6','2','3','0','1','0','2','0','2'};
 
     c = toupper(c);
 
-    for(int i =0; i< sizeof(codearr);i++)
+    while(i < 26)
     {
         if(c == chararr[i])
         {
             res = codearr[i];
+            break;
         }
-    }
+        i++;
+    };
 
     return res;
 }
 
-void generateSoundex(const char *name, char *soundex) {
+char checkcode(char code,char *soundex,int sIndex)
+{
+    if(code != '0' && code != soundex[sIndex - 1])
+    {
+        return code;
+    }
+    else
+    {
+        return 0;
+    }
+}
+void generateSoundex(const char *name, char *soundex)
+{
     soundex[0] = toupper(name[0]);
     int sIndex = 1;
 
-    for (int i = 1; i < 4; i++) {
+    for (int i = 1; i < 4; i++)
+    {
         char code = getSoundexCode(name[i]);
-        if (code != '0' && code != soundex[sIndex - 1]) {
+        if (checkcode(code,soundex,sIndex))
+        {
             soundex[sIndex++] = code;
         }
     }
@@ -41,4 +57,5 @@ void generateSoundex(const char *name, char *soundex) {
 
     soundex[4] = '\0';
 }
+
 #endif // SOUNDEX_H
