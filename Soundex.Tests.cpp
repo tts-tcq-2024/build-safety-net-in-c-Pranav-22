a8adb00b16bbafe233affb18be99b3ac7a8fc032
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-07-03 13:23:11 +0530

    added test cases

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -7,3 +7,32 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   generateSoundex("AX", soundex);
   //ASSERT_EQ(soundex,"A200");
 }
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits)
+{
+  char soundex[5];
+  generateSoundex("X0", soundex);
+}
+
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits)
+{
+  char soundex[5];
+  generateSoundex("ZQOP", soundex);
+}
+
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits)
+{
+  char soundex[5];
+  generateSoundex("ASDSD", soundex);
+}
+
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits)
+{
+  char soundex[5];
+  generateSoundex("12345", soundex);
+}
+
+
