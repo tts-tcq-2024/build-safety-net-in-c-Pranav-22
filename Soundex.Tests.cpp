e7c354422990abde48ba3eccb9a6383485648fc9
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-19 11:26:33 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -8,31 +8,35 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   EXPECT_STREQ(soundex,"A200");
 }
 
-TEST(SoudexTestsuite1, ReplacesConsonantsWithAppropriateDigits1)
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits1)
 {
   char soundex[5];
   generateSoundex("X0", soundex);
+ EXPECT_STREQ(soundex,"X000");
 }
 
 
-TEST(SoudexTestsuite2, ReplacesConsonantsWithAppropriateDigits2)
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits2)
 {
   char soundex[5];
   generateSoundex("ZQOP", soundex);
+ EXPECT_STREQ(soundex,"Z210");
 }
 
 
-TEST(SoudexTestsuite3, ReplacesConsonantsWithAppropriateDigits3)
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits3)
 {
   char soundex[5];
   generateSoundex("ASDSD", soundex);
+ EXPECT_STREQ(soundex,"A232");
 }
 
 
-TEST(SoudexTestsuite4, ReplacesConsonantsWithAppropriateDigits4)
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits4)
 {
   char soundex[5];
   generateSoundex("12345", soundex);
+ EXPECT_STREQ(soundex,"1000");
 }
 
 
